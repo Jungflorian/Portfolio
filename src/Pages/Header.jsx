b761af3982c229653a496cad069a008148b7d4e1
Jungflorian <jungflorian@outlook.fr>
2025-08-19 09:51:03 +0200

    expo portfolio

---
+++ src/Pages/Header.jsx
@@ -0,0 +1,16 @@
+import '../CSS/Header.css';
+function Header() {
+  return (
+    <div className="header">
+      <h1>Mon portfolio </h1>
+      <ul>
+        <li>Présentation</li>
+        <li>Compétences</li>
+        <li>Projets</li>
+        <li>Me contacter</li>
+      </ul> 
+    </div>
+  );
+}
+
+export default Header;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  base: '/Portfolio/', // Remplacez <nom-du-repo> par le nom de votre dépôt GitHub
+  base: '/Portfolio/', 
   plugins: [react()],
 })
